3b875ceac919e9be48c4f4e7bfd3aa25e255ac40
Matt Smart <mdsmart36@gmail.com>
2015-07-14 10:13:18 -0500

    finished cheers

---
+++ csharp/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string article;
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            // Prompt user to type a name
+            Console.Write("Please enter your name: ");
+            string input = Console.ReadLine();
+            
+
+            // print out the cheer
+            // for each character in the name
+            //     print out the line for the appropriate cheer
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                article = Array.IndexOf(vowels, input[i]) < 0 ? "a" : "an";
+                Console.WriteLine("Give me {0}..{1}", article, input[i]);
+            }
+            
+            // print out final line with name in uppercase
+            Console.WriteLine("{0} is.. GRAND", input.ToUpper());
+
+            Console.ReadLine();
+        }
+    }
+}
