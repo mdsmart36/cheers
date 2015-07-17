746335ed4df732ed4bf9cc14891f779b47e14b47
Matt Smart <mdsmart36@gmail.com>
2015-07-17 16:27:50 -0500

    more refactoring

--- csharp/ConsoleApplication1/Program.cs
+++ csharp/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleApplication1
 {
@@ -13,37 +14,32 @@ namespace ConsoleApplication1
         public static void Main(string[] args)
         {
             string article; // "a" or "an"
-            char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' }; // array of vowels
-
+            string vowels = "aeiou";
+            
             // Prompt user to type a name and birthday
             Console.Write("Please enter your name: ");
-            string input = Console.ReadLine();
-            input = input.ToLower();
+            string name = Console.ReadLine();
+            name = name.ToLower();
 
             Console.Write("What's your birthday? (MM/DD) ");
-            string bday = Console.ReadLine();
+            string birthday = Console.ReadLine();
             Console.WriteLine();
-
-            // parse string into month and day
-            string[] monthAndDay = bday.Split('/');
-
-            // create DateTime object for birthday (Year, Month, Day)
-            DateTime birthday = new DateTime(DateTime.Today.Year, int.Parse(monthAndDay[0]), int.Parse(monthAndDay[1]));
-
-            // calculate difference between today and next birthday
-            int difference = DateTime.Today.Subtract(birthday).Days;
+            
+            // calculate number of days from today until next birthday
+            int difference = DateTime.Today.Subtract(DateTime.Parse(birthday)).Days;
+            string daysUntilBirthday = getDaysUntilBirthday(difference);
             
             // print out the cheer with appropriate "a" or "an" if the letter is a consonant or vowel
-            foreach (var letter in input)
+            foreach (char letter in name)
             {
-                article = Array.IndexOf(vowels, letter) < 0 ? "a" : "an";
+                article = vowels.Contains(letter) ? "an" : "a";
                 Console.WriteLine("Give me {0}..{1}", article, letter);
             }
             
-            Console.WriteLine("\n{0} is.. GRAND\n", input.ToUpper());
+            Console.WriteLine("\n{0} is.. GRAND\n", name.ToUpper());
             
             Console.WriteLine( (difference == 0) ? "Happy Birthday!!" : 
-                "Your birthday is {0} days away.", getDaysUntilBirthday(difference) );
+                "Your birthday is {0} days away.", daysUntilBirthday );
 
             Console.ReadLine();
         }
